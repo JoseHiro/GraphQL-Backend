b2bd6ab6a1bce70e42e7df78302b295fb03ca601
Josey Sekine <ipho.7johero@icloud.com>
2023-03-23 17:28:32 -0700

    practiced graphql

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const path = require('path');
+const fs = require('fs')
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
-const dotenv = require('dotenv');
+const {graphqlHTTP} = require('express-graphql');
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolver = require('./graphql/resolver');
+const auth = require('./middleware/auth');
+const { clearImage } = require('./util/file');
 
+const dotenv = require('dotenv');
 dotenv.config('.env');
 const MONGO_DB = process.env.MONGO_KEY;
-
-const feedRoutes = require('./routes/feed');
-const authRoutes = require('./routes/auth');
 
 const app = express();
 
@@ -49,11 +52,47 @@ app.use((req, res, next) => {
     'OPTIONS, GET, POST, PUT, PATCH, DELETE'
   );
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if(req.method === 'OPTIONS'){
+    return res.sendStatus(200);
+  }
   next();
 });
 
-app.use('/feed', feedRoutes);
-app.use('/auth', authRoutes);
+app.use(auth);
+
+app.put('/post-image', (req, res, next) => {
+  if (!req.isAuth) {
+    throw new Error('Not authenticated!');
+  }
+  if (!req.file) {
+    return res.status(200).json({ message: 'No file provided!' });
+  }
+  if (req.body.oldPath) {
+    clearImage(req.body.oldPath);
+  }
+  return res
+    .status(201)
+    .json({ message: 'File stored.', filePath: req.file.path });
+});
+
+
+app.use(
+  '/graphql',
+  graphqlHTTP ({
+  schema: graphqlSchema,
+  rootValue : graphqlResolver,
+  graphiql: true,
+  formatError(err){
+    if(!err.originalError){
+      return err
+    }
+    const data = err.originalError.data;
+    const message = err.message || 'An error occured.';
+    const code = err.originalError.code || 500;
+    return { message: message, status: code, data: data}
+  }
+}));
+
 
 app.use((error, req, res, next) => {
   console.log(error);
@@ -68,10 +107,6 @@ mongoose
     MONGO_DB
   )
   .then(result => {
-    const server = app.listen(8080);
-    const io = require('./socket').init(server);
-    io.on('connection', socket => {
-      console.log('Client connected');
-    });
+    app.listen(8080);
   })
   .catch(err => console.log(err));
